6cc9e14e46234d02e6b261f572e0f40b4834f3a5
Phoebe Valdes <56165012+phoebe460@users.noreply.github.com>
2023-09-06 11:28:07 -0700

    Files are updated.

---
+++ Trimgalore_RNA_EOAD.NDC.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+#This script runs trimgalore.
+#For RNA-Seq Chen Foundation Samples
+
+#Change to the directory containing all the RNA-Seq samples in FASTQ format (.fastq.gz)
+cd ~/Chen_Foundation_FASTQ
+
+#Load the fastqc module
+module load fastqc
+
+# loop for all the samples
+for j in {1..27};
+do
+	FILENAME=`sed -n ''$j'p' ~/EOAD.NDC_samples_info.txt` 
+	echo $FILENAME
+	#Run trimgalore
+	~/programs/TrimGalore-0.6.5/trim_galore \
+	--quality 25 \
+	--fastqc \
+        --illumina \
+	--length 98 \
+        --paired \
+	--cores 4 \
+	-o ~/clean_Chen_Foundation_FASTQ_files \
+	${FILENAME}_R1_001.fastq.gz ${FILENAME}_R2_001.fastq.gz
+done
